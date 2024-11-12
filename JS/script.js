3c8632555cbf98864ec631220b082092335921e9
Ponie25 <silkyy2507@gmail.com>
2024-11-12 17:17:41 +0700

    Second commit

--- JS/script.js
+++ JS/script.js
@@ -4,10 +4,5 @@ const speechBubble = document.getElementById("speechBubble");
 
 // Add a click event listener to toggle the speech bubble visibility
 plane.addEventListener("click", function() {
-    // Toggle visibility
-    if (speechBubble.classList.contains("hidden")) {
-        speechBubble.classList.remove("hidden");
-    } else {
-        speechBubble.classList.add("hidden");
-    }
-});
+    
+}
